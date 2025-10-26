c01e483af019270cbc76a697d374808e6ba285a4
MayerlinLemosBedoya <maleiquianchico@hotmail.com>
2025-10-26 10:15:00 -0500

    frontend: read WalletConnect projectId from VITE_WALLET_CONNECT_PROJECT_ID

--- src/config/wagmi.js
+++ src/config/wagmi.js
@@ -1,9 +1,11 @@
 import { getDefaultConfig } from '@rainbow-me/rainbowkit'
 import { baseSepolia } from 'wagmi/chains'
 
+const projectId = import.meta.env.VITE_WALLET_CONNECT_PROJECT_ID || ''
+
 export const config = getDefaultConfig({
   appName: 'AlgaeOS',
-  projectId: 'YOUR_WALLET_CONNECT_PROJECT_ID', // Get from https://cloud.walletconnect.com
+  projectId,
   chains: [baseSepolia],
   ssr: false, // If your dApp uses server side rendering (SSR)
 })
